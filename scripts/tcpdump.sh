3f6486d07e8a241bac9ed57e5c7ca114e4a19d3d
Timothy Mossey <tim@pwnieexpress.com>
2015-04-06 13:35:43 -0400

    add shield device detection

--- scripts/tcpdump.sh
+++ scripts/tcpdump.sh
@@ -3,14 +3,13 @@
 
 ##################################################
 f_identify_device(){
-
-# Check device
+  # Check device
   hardw=`getprop ro.hardware`
   if [[ "$hardw" == "deb" || "$hardw" == "flo" ]]; then
-    # Set interface for new Pwn Pad
+    # Set interface for Pwn Pad 2
     gsm_int="rmnet_usb0"
   else
-    # Set interface for Pwn Phone and old Pwn Pad
+    # Set interface for Pwn Pad 1, Pwn Pad 3 and Pwn Phone
     gsm_int="rmnet0"
   fi
 }
